922710d4e8c22d968a54308c2da7b9c25690c456
Great Lucky <lgreat098@gmail.com>
2025-11-10 03:32:04 +0100

    modify the search header component

--- app/components/SearchHeader.tsx
+++ app/components/SearchHeader.tsx
@@ -1,5 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+import { SearchBox } from "./SearchBox";
+import { IoSettingsOutline } from "react-icons/io5";
+import { CgMenuGridO } from "react-icons/cg";
+import { SearchHeaderOptions } from "./SearchHeaderOptions";
 function SearchHeader() {
-  return <div>Search</div>;
+  return (
+    <header className="stick top-0 bg-white">
+      <div className="flex w-full p-6 items-center justify-between">
+        <Link href={"/"}>
+          <Image
+            src={`https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png`}
+            width={120}
+            height={40}
+            alt="google"
+            loading="eager"
+            style={{ height: "auto", width: "auto" }}
+          />
+        </Link>
+        <div className="flex-1">
+          <SearchBox />
+        </div>
+        <div className="hidden md:inline-flex space-x-2">
+          <IoSettingsOutline className="header-icon" />
+          <CgMenuGridO className="header-icon" />
+        </div>
+        <button className="text-white bg-blue-700 px-6 py-2 font-medium rounded-full hover:brightness-105 hover:shadow-md transition-all cursor-pointer ml-2">
+          Sign in
+        </button>
+      </div>
+      <SearchHeaderOptions />
+    </header>
+  );
 }
 
 export { SearchHeader };
---
+++ app/components/SearchHeaderOptions.tsx
@@ -0,0 +1,5 @@
+function SearchHeaderOptions() {
+  return <div>SHO</div>;
+}
+
+export { SearchHeaderOptions };
